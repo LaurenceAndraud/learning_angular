bc8504adbdbf03132c4c996dd4e4322bd8a31c9e
LaureAND <110863447+LaureAND@users.noreply.github.com>
2024-10-09 18:15:45 +0200

    front changes with schema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { BookListComponent } from './book-list/book-list.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
@@ -15,12 +20,24 @@ import { HttpClientModule } from '@angular/common/http';
     HomeComponent,
     BookListComponent, 
     LoginComponent, 
-    RegisterComponent], 
+    RegisterComponent,
+    HeaderComponent,
+    FooterComponent,
+    MatButtonModule,
+    MatCardModule,
+  ], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'gestion-book';
+
+  constructor(private router: Router) {}
+
+  navigateTo(path: string): void {
+    this.router.navigate([path]);
+  }
 }
+
 
 
